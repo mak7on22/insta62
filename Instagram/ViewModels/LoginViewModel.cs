cac763bc98e083999310c09520991895ed80738c
mak7on22 <mur-mur-0998@mail.ru>
2023-10-23 10:13:36 +0600

    Added previous instagram ViewModels Login and Register

---
+++ Instagram/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Instagram.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email или логин ")]
+        public string? Email { get; set; }
+        [Display(Name = "Логин или Email")]
+        public string? Login { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+        public string? ReturnUrl { get; set; }
+
+        public LoginViewModel() 
+        {
+            Login = "";
+        }
+    }
+}
---
+++ Instagram/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Instagram.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Логин")]
+        public string? Login { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Аватар")]
+        public string? Avatar { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "Имя")]
+        public string? Name { get; set; }
+        [Required]
+        [Display(Name = "Краткая информация")]
+        public string? UserInfo { get; set; }
+        [Required]
+        [Display(Name = "Ваш номер")]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [Display(Name = "Ваш пол")]
+        public string? Sex { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords is not the same")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Повторите пароль")]
+        public string PasswordConfirm { get; set; }
+
+    }
+}

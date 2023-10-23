1dacefc7b2673b08b3e6fc9548132dbf2e3a98c5
mak7on22 <mur-mur-0998@mail.ru>
2023-10-23 10:14:32 +0600

    Added the previous account controller

---
+++ Instagram/Controllers/AccountController.cs
@@ -0,0 +1,121 @@
+﻿using Instagram.Models;
+using Instagram.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Instagram.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private ILookupNormalizer normalizer;
+        IWebHostEnvironment _appEnvironment;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, IWebHostEnvironment appEnvironment)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _appEnvironment = appEnvironment;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model, [FromForm(Name = "uploadedFile")] IFormFile uploadedFile)
+        {
+            string path = "/Files/" + uploadedFile.FileName;
+            using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+            {
+                await uploadedFile.CopyToAsync(fileStream);
+            }
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    Login = model.Login,
+                    Email = model.Email,
+                    Name = model.Name,
+                    UserName = model.Login,
+                    Avatar = path,
+                    UserInfo = model.UserInfo,
+                    PhoneNumber = model.PhoneNumber,
+                    Sex = model.Sex,
+                    PasswordHash= model.Password
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Publication");
+                }
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = null;
+                if (!string.IsNullOrEmpty(model.Email))
+                    user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    user = await _userManager.FindByNameAsync(model.Email);
+                    if (user != null)
+                    {
+                        user.Login = model.Email;
+                        await _userManager.UpdateAsync(user);
+                    }
+                }
+                if (user != null)
+                {
+                    Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(
+                        user,
+                        model.Password,
+                        model.RememberMe,
+                        false
+                    );
+                    if (result.Succeeded)
+                    {
+                        if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                            return Redirect(model.ReturnUrl);
+                        else
+                            return RedirectToAction("Index", "Publication");
+                    }
+                }
+                ModelState.AddModelError("", "Неправильный логин и/или пароль");
+            }
+            return View(model);
+        }
+
+
+
+
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Publication");
+        }
+    }
+}
